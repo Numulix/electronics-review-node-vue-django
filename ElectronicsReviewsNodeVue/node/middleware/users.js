96d3c725f538ebe176cc6e365d23b4da6a759481
Jovan Babic <jbabic999@gmail.com>
2021-02-13 21:04:34 +0100

    Joi backend validacione seme

--- ElectronicsReviewsNodeVue/node/middleware/users.js
+++ ElectronicsReviewsNodeVue/node/middleware/users.js
@@ -49,6 +49,11 @@ module.exports = {
     isAdmin: (req, res, next) => {
         try {
             const token = req.headers.authorization.split(' ')[1];
+            if (!token) {
+                res.status(403).send({
+                    message: "Unauthorized"
+                })
+            }
             const user_id = jwt.verify(
                 token,
                 'SECRETKEY'
--- ElectronicsReviewsNodeVue/node/routes/router.js
+++ ElectronicsReviewsNodeVue/node/routes/router.js
@@ -3,10 +3,32 @@ const router = express.Router();
 
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const Joi = require("joi");
 
 const db = require("../lib/db.js");
 const userMiddleware = require("../middleware/users.js");
 const { reset } = require("nodemon");
+
+// Model schemas for Joi validation
+
+const categorySchema = Joi.object().keys({
+  name: Joi.string().min(3).max(30).required()
+});
+
+const productSchema = Joi.object().keys({
+  product_name: Joi.string().min(5).max(50).required(),
+  category_id: Joi.number().integer().required(),
+  product_description: Joi.string().min(5).max(1024),
+  price: Joi.number().integer().min(1).required()
+});
+
+const productReviewSchema = Joi.object().keys({
+  user_id: Joi.number().integer().required(),
+  product_id: Joi.number().integer().required(),
+  review_text: Joi.string().min(5).max(1024).required()
+});
+
+// USER REGISTRATION AND LOGIN HANDLING
 
 router.post("/sign-up", userMiddleware.validateRegister, (req, res, next) => {
   db.query(
@@ -111,14 +133,16 @@ router.post("/login", (req, res, next) => {
 });
 
 router.get("/test-auth-route", userMiddleware.isLoggedIn, (req, res, next) => {
-    console.log(req.userData);
-    res.send('This is a test auth route');
+  console.log(req.userData);
+  res.send("This is a test auth route");
 });
 
 router.get("/test-admin-route", userMiddleware.isAdmin, (req, res, next) => {
-    res.send({
-      message: "You're an admin!"
-    })
+  res.send({
+    message: "You're an admin!",
+  });
 });
+
+// MODEL HANDLING
 
 module.exports = router;
